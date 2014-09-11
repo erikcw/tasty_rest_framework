55f72772b61618d35e26e47a04492c6b95edcb70
Erik Wickstrom <erik@erikwickstrom.com>
2014-09-11 14:38:11 -0700

    Initial code commit.

---
+++ tasty_rest_framework/authentication.py
@@ -0,0 +1,50 @@
+from rest_framework import authentication
+from rest_framework import exceptions
+from rest_framework.authtoken.models import Token
+
+
+class TastyApiKeyAuthentication(authentication.TokenAuthentication):
+    """
+    Use TastyPie's ApiKeyAuthentication scheme with Django Rest Framework.
+
+    The model property can point to either a legacy tastypie.models.ApiKey model
+    or you can migrate your API keys to rest_framework.authtoken.models.Token.
+    The latter is recommended if you are planning to remove tastypie from your codebase
+    entirely.
+    """
+
+    model = Token
+
+    def authenticate(self, request):
+        auth = authentication.get_authorization_header(request).split()
+
+        if not auth or auth[0].lower() != b'apikey':
+            return None
+
+        if len(auth) == 1:
+            msg = 'Invalid token header. No credentials provided.'
+            raise exceptions.AuthenticationFailed(msg)
+        elif len(auth) > 2:
+            msg = 'Invalid token header. Token string should not contain spaces.'
+            raise exceptions.AuthenticationFailed(msg)
+
+        try:
+            username, key = auth[1].split(':')
+        except ValueError:
+            raise exceptions.AuthenticationFailed('Invalid username token pair')
+
+        return self.authenticate_credentials(username, key)
+
+    def authenticate_credentials(self, username, key):
+        try:
+            token = self.model.objects.get(user__username=username, key=key)
+        except self.model.DoesNotExist:
+            raise exceptions.AuthenticationFailed('Invalid token')
+
+        if not token.user.is_active:
+            raise exceptions.AuthenticationFailed('User inactive or deleted')
+
+        return (token.user, token)
+
+    def authenticate_header(self, request):
+        return 'ApiKey'
---
+++ tasty_rest_framework/mixins.py
@@ -0,0 +1,50 @@
+from .pagination import TastyPaginationSerializer
+
+
+class TastyPieViewMixin(object):
+    """
+    Use this mixin with Views and Viewsets to achieve TastyPie like
+    funcionality.
+    """
+    pagination_serializer_class = TastyPaginationSerializer
+
+    paginate_by_param = 'limit'
+    paginate_offset_param = 'offset'
+
+    def paginate_queryset(self, queryset, page_size=None):
+        """
+        Tastypie style pagination.
+        """
+        page = super(TastyPieViewMixin, self).paginate_queryset(queryset, page_size=page_size)
+
+        if page is None:
+            return page
+
+        offset = self.request.GET.get(self.paginate_offset_param)
+        if offset:
+            # convert cursor into page number and fetch page
+            limit = page.paginator.per_page
+            page_number = (int(offset) / limit) + 1
+            page = page.paginator.page(page_number)
+
+        return page
+
+
+class TastyPieSerializerMixin(object):
+    """
+    Modify serializer to behave like TastyPie
+    """
+
+    def __init__(self, *args, **kwargs):
+        self._update_meta()
+        super(TastyPieSerializerMixin, self).__init__(*args, **kwargs)
+
+    def _update_meta(self):
+        """
+        Update the Serializer's inner Meta class settings with those needed
+        for TastyPie compatibility.
+        """
+        error_msg = "{0} already has {1} set on its Meta class. Remove it to continue."
+        attr_name = 'url_field_name'
+        assert hasattr(self.Meta, attr_name) is False, error_msg.format(repr(self), attr_name)
+        setattr(self.Meta, attr_name, 'resource_uri')
