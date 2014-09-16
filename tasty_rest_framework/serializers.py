d544d355f40b8eb5f153570ca626b26b586e95f2
Erik Wickstrom <erik@erikwickstrom.com>
2014-09-15 17:58:06 -0700

    Created a TastyPie style HyperlinkedRelatedField subclass.

--- tasty_rest_framework/serializers.py
+++ tasty_rest_framework/serializers.py
@@ -1,10 +1,20 @@
-from rest_framework.relations import HyperlinkedIdentityField
+from rest_framework.relations import HyperlinkedIdentityField, HyperlinkedRelatedField
 import urllib2
 
 class TastyPieHyperlinkedIdentityField(HyperlinkedIdentityField):
 
     def get_url(self, obj, view_name, request, format):
         url = super(TastyPieHyperlinkedIdentityField, self).get_url(obj, view_name, request, format)
+        # make a relative url by removing the scheme and host.
+        parts = urllib2.urlparse.urlsplit(url)
+        url = urllib2.urlparse.urlunsplit((None, None, parts.path, parts.query, parts.fragment,))
+        return url
+
+
+class TastyPieHyperlinkedRelatedField(HyperlinkedRelatedField):
+
+    def get_url(self, obj, view_name, request, format):
+        url = super(TastyPieHyperlinkedRelatedField, self).get_url(obj, view_name, request, format)
         # make a relative url by removing the scheme and host.
         parts = urllib2.urlparse.urlsplit(url)
         url = urllib2.urlparse.urlunsplit((None, None, parts.path, parts.query, parts.fragment,))
